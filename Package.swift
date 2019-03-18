2326b65267a49bf1f9b19352dced39f7fe419542
zhenghuiwin <bluegene.hao@gmail.com>
2019-03-18 19:18:25 +0800

    删除了对Chndf的使用

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
     pkgConfig: "netcdf",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // .package(url: "../Chdf5", from: "0.0.3"),
     ]
 )
